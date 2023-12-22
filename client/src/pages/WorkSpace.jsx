bd54036dce1a39df09bbe6d4dac1b7f3575a0c11
Devendra-Kumar-Dhayal <101494090+Devendra-Kumar-Dhayal@users.noreply.github.com>
2023-12-22 07:18:01 +0530

    feat bpmn-js added

--- client/src/pages/WorkSpace.jsx
+++ client/src/pages/WorkSpace.jsx
@@ -1,18 +1,50 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
-import { TLUiOverrides,Tldraw, track, useEditor } from '@tldraw/tldraw'
-import '@tldraw/tldraw/tldraw.css'
-
-import CustomUi from "./customUI";
 
 
-const WorkSpace = () => {
+
+
+
+
+import jsPlumb from '@jsplumb/browser-ui';
+
+const Workspace = () => {
+  const editorRef = useRef(null);
+
+  useEffect(() => {
+    if (editorRef.current) {
+      jsPlumb.ready(function () {
+        // Initialize jsPlumb
+        const instance = jsPlumb.getInstance({
+          Container: editorRef.current,
+          Connector: ['Bezier', { curviness: 150 }],
+        });
+
+        // Make elements draggable
+        instance.draggable(document.querySelectorAll('.box'), {
+          grid: [20, 20],
+        });
+
+        // Connect elements
+        instance.connect({ source: 'box1', target: 'box2' });
+      });
+    }
+  }, []);
+
   return (
-		<div style={{ position: 'fixed', inset: 0 }}>
-			<Tldraw >
-				<CustomUi></CustomUi>
-			</Tldraw>
-		</div>
-	)
-}
+    <div>
+      <div id="editor" ref={editorRef}>
+        <div id="box1" className="box" style={{ left: '50px', top: '50px' }}>
+          Box 1
+        </div>
+        <div id="box2" className="box" style={{ left: '200px', top: '100px' }}>
+          Box 2
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default WorkSpace;
+export default Workspace;
+
+
+
