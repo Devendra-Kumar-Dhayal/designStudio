5ca5677897bd762efa3428c5090ea4609d7471c5
Devendra-Kumar-Dhayal <101494090+Devendra-Kumar-Dhayal@users.noreply.github.com>
2024-01-01 09:37:21 +0530

    main

--- server/config/custom-environment-variables.ts
+++ server/config/custom-environment-variables.ts
@@ -3,7 +3,7 @@ export default {
   accessTokenPublicKey: "ACCESS_TOKEN_PUBLIC_KEY",
   refreshTokenPrivateKey: "REFRESH_PRIVATE_KEY",
   refreshTokenPublicKey: "REFRESH_PUBLIC_KEY",
-  dbUri: "DB_CONNECTION",
+  dbUri: "mongodb://localhost:27017/design-studio",
   port: "PORT",
   googleClientId: "GOOGLE_CLIENT_ID",
   googleClientSecret: "GOOGLE_CLIENT_SECRET",
